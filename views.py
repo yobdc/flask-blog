44c243633bdf58d58a6a86eba9ca9f12a66f8751
Lex <yobdc.101@gmail.com>
2016-12-13 18:26:37 +0800

    fix typo

--- views.py
+++ views.py
@@ -5,9 +5,20 @@ from flask import render_template, session, redirect, url_for, request
 from models import db, app
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 @app.before_request
 def before_request():
-    if (request.path != '/' and not '/api' in request.path and not session.has_key('username')):
+    if (request.path != '/'
+        and not '/api' in request.path
+        and not request.path.endswith('.js')
+        and not request.path.endswith('.css')
+        and not request.path.endswith('.jpg')
+        and not request.path.endswith('.png')
+        and not session.has_key('username')):
         return redirect(url_for('index'))
 
 
